92b350b43edcad1b23c28b3d62e00b224f709365
MINHDANGab <thaotrau55@gmail.com>
2024-12-05 11:12:35 +0700

    Them Folder View ViewModel

---
+++ KTPMUD/ViewModel/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTPMUD.ViewModel
+{
+    internal class UserViewModel
+    {
+    }
+}
